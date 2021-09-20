f15e3e2196373c13c5a6973d009a3ed0fc172554
sehajdeep1814 <88536163+sehajdeep1814@users.noreply.github.com>
2021-09-20 14:23:25 +0530

    Update Extraction.py
    Formation of a new 'data' variable to store all the extracted data.

--- Extraction.py
+++ Extraction.py
@@ -1,3 +1,6 @@
+
+#News- Extractioin
+
 #Step 0: Importing packages
 
 import requests
@@ -20,35 +23,47 @@ soup = BeautifulSoup(content,'html.parser')
 
 all_news = soup.find_all('div',{'class':'news-card z-depth-1'})
 
+news_headlines=[]
+news_author = []
+news_body = []
+news_images = []
+news_url = []
+news_keyword =[]
+
+
 for news in all_news:
-    news_headlines = news.find('span',{'itemprop':'headline'}).text.strip()#Headline
-    news_author = news.find('span',{'class':'author'}).text.strip()#Author
-    news_body = news.find('div',{'itemprop':'articleBody'}).text.strip()#Body
-    anchor = news.find_all('a',{'class':'source'})
-    news_url = ''
-    for link in anchor:
-        if(link.get('href')!='#'):
-            news_url = link.get('href')#News Source
-    images = news.find_all('div',{'class':'news-card-image'})
-    news_images = ''
-    for im in images:
-        news_images = im.get('style')[23:-3]#Image url
-    news_headlines_1 = news_headlines.replace('.','').replace(',',"").split(' ')
-    news_body_1 = news_body.replace('.',' ').replace(',',"").split(" ")
+
+    headline = news.find('span',{'itemprop':'headline'}).text.strip()
+    news_headlines.append(headline)#Headline
+    
+    news_author.append(news.find('span',{'class':'author'}).text.strip())#Author
+    
+    body = news.find('div',{'itemprop':'articleBody'}).text.strip()
+    news_body.append(body)#Body
+    
+    anchor = news.find('a',{'class':'source'})
+    if(anchor != None):
+        news_url.append(anchor.get('href'))#News Source
+    else:
+        news_url.append('')
+    
+    images = news.find('div',{'class':'news-card-image'})
+    news_images.append(images.get('style')[23:-3])#Image url
+    
+    news_headlines_1 = headline.replace('.','').replace(',',"").split(' ')
+    
+    news_body_1 = body.replace('.',' ').replace(',',"").split(" ")
+    
     max_occurance = []
     for i in news_headlines_1:
-        if i not in ['a','an','the','in','is','of','on','to','for','after','I',"I'm",'be','he','she','they','him','her','are']:
+        if i not in ['a','an','the','in','is','of','my','no','yes','on','to','for','it','after','I',"I'm",'be','he','she','are','they','him','her']:
             max_occurance.append(news_body_1.count(i))
         else:
             max_occurance.append(0)
-    news_keyword = news_headlines_1[max_occurance.index(max(max_occurance))]# News Keyword
-    print('Headlines: ',news_headlines,'.\n','Description: ',news_body,'\n','Keyword- ',news_keyword,'\n','Author- ',news_author,'\n','Source: ',news_url,'\n','Top Image link: ',news_images,'\n\n')
     
+    news_keyword.append(news_headlines_1[max_occurance.index(max(max_occurance))])# News Keyword
     
 
-all_categories = soup.find_all('a')
-print(" News Categories:\n")
-for categories in all_categories:
-    if(categories.get('href').startswith('/en/read/')):
-        category_url = 'https://www.inshorts.com'+ categories.get('href')#Url for categories
-        print(category_url)
+
+data = [news_headlines,news_author,news_body,news_url,news_images,news_keyword]
+news_length = len(data[0])
