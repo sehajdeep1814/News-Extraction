1ab1440e71cbd7e26ccd5cbd68e1c6822ab6940d
Qasim <shaikhmq20.comp@coep.ac.in>
2022-02-19 19:43:25 +0530

    Changing the look of page

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, url_for
 from Extraction import data, news_length
 
 app = Flask('__name__')
 
+
 @app.route('/')
 def homepage():
-    return render_template('homepage.html',data=data,news_length= news_length)
+    return render_template('homepage.html', data=data, news_length=news_length)
+
 
 if __name__ == '__main__':
-    app.run(debug= True)
+    app.run(debug=True)
