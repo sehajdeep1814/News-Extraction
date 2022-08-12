9827039cee86a6ace38a564f7b34d99bd8a2141e
Qasim Shaikh <shaikhmq20.comp@coep.ac.in>
2022-08-12 15:31:47 +0530

    adding categories for news

--- Extraction.py
+++ Extraction.py
@@ -7,21 +7,23 @@ import requests
 
 from bs4 import BeautifulSoup
 
-url = "https://www.inshorts.com/en/read"
+categories = {
+    "all News": "",
+    "india": "national",
+    "business": "business",
+    "sports": "sports",
+    "world": "world",
+    "politics": "politics",
+    "technology": "technology",
+    "entertainment": "entertainment",
+    "science": "science",
+}
 
-# Step 1: GET HTML using requests
-
-r = requests.get(url)
-
-content = r.content
-
-# Step 2: Parsing HTML using BeautifulSoup
-
-soup = BeautifulSoup(content, 'html.parser')
-
-# Step 3:  HTML Tree traversal
-
-all_news = soup.find_all('div', {'class': 'news-card z-depth-1'})
+common_words = []
+# Adding 3000 common words in English in the common_words list
+with open("words.txt") as file:
+    for words in file.readlines():
+        common_words.append(words[:-1])
 
 class News:
     def __init__(self, tag, attribute):
@@ -46,44 +48,60 @@ class News:
             image = single_news.find(self.tag, self.attribute)
             self.items.append(image.get('style')[23:-3])
 
+url = ""
+news_categories = {}
+for label, link in categories.items():
+    url = f"https://www.inshorts.com/en/read/{link}"
+
+    # Step 1: GET HTML using requests
+
+    r = requests.get(url)
+
+    content = r.content
+
+    # Step 2: Parsing HTML using BeautifulSoup
+
+    soup = BeautifulSoup(content, 'html.parser')
+
+    # Step 3:  HTML Tree traversal
+
+    all_news = soup.find_all('div', {'class': 'news-card z-depth-1'})
 
 
-headlines = News('span', {'itemprop': 'headline'})
-headlines.add(all_news)
 
-authors = News('span', {'class': 'author'})
-authors.add(all_news)
 
-bodies = News('div', {'itemprop': 'articleBody'})
-bodies.add(all_news)
+    headlines = News('span', {'itemprop': 'headline'})
+    headlines.add(all_news)
 
-urls = News('a', {'class': 'source'})
-urls.addURLs(all_news)
+    authors = News('span', {'class': 'author'})
+    authors.add(all_news)
 
-images = News('div', {'class': 'news-card-image'})
-images.addImages(all_news)
+    bodies = News('div', {'itemprop': 'articleBody'})
+    bodies.add(all_news)
 
-news_keyword = []
-common_words = []
+    urls = News('a', {'class': 'source'})
+    urls.addURLs(all_news)
 
-# Adding 3000 common words in English in the common_words list
-with open("words.txt") as file:
-    for words in file.readlines():
-        common_words.append(words[:-1])
+    images = News('div', {'class': 'news-card-image'})
+    images.addImages(all_news)
 
-for i in range(len(headlines.items)):
-    max_occurance = []
-    headlines_words = headlines.items[i].replace('.', '').replace(',', '').split(' ')
-    bodies_words = bodies.items[i].replace('.', '').replace(',', '').split(' ')
+    news_keyword = []
 
-    for word in headlines_words:
-        if word not in common_words:
-            max_occurance.append(bodies.items.count(word))
-        else:
-            max_occurance.append(0)
-    
-    news_keyword.append(headlines_words[max_occurance.index(max(max_occurance))])
 
-data = [headlines.items, authors.items, bodies.items,
-        urls.items, images.items, news_keyword]
-news_length = len(data[0])
+
+    for i in range(len(headlines.items)):
+        max_occurrence = []
+        headlines_words = headlines.items[i].replace('.', '').replace(',', '').split(' ')
+        bodies_words = bodies.items[i].replace('.', '').replace(',', '').split(' ')
+
+        for word in headlines_words:
+            if word not in common_words:
+                max_occurrence.append(bodies.items.count(word))
+            else:
+                max_occurrence.append(0)
+
+        news_keyword.append(headlines_words[max_occurrence.index(max(max_occurrence))])
+
+    data = [headlines.items, authors.items, bodies.items,
+            urls.items, images.items, news_keyword]
+    news_categories[link] = data;
--- test.py
+++ test.py
@@ -1,12 +1,59 @@
 from flask import Flask, render_template, url_for
-from Extraction import data, news_length
+from Extraction import data, news_categories
 
 app = Flask('__name__')
 
 
+# for link, data in news_categories.items():
+#     url = f"/{link}"
+#     @app.route(url)
+#     def render_page():
+#         return render_template('homepage.html', data=data, news_length=len(data[0]))
+
 @app.route('/')
-def homepage():
-    return render_template('homepage.html', data=data, news_length=news_length)
+def allNews():
+    data = news_categories[""]
+    return render_template('homepage.html', data=data, news_length=len(data[0]))
+
+@app.route('/national')
+def national():
+    data = news_categories["national"]
+    return render_template('homepage.html', data=data, news_length=len(data[0]))
+
+@app.route('/business')
+def business():
+    data = news_categories["business"]
+    return render_template('homepage.html', data=data, news_length=len(data[0]))
+
+@app.route("/sports")
+def sports():
+    data = news_categories["sports"]
+    return render_template('homepage.html', data=data, news_length=len(data[0]))
+
+@app.route("/world")
+def world():
+    data = news_categories["world"]
+    return render_template('homepage.html', data=data, news_length=len(data[0]))
+
+@app.route("/politics")
+def politics():
+    data = news_categories["politics"]
+    return render_template('homepage.html', data=data, news_length=len(data[0]))
+
+@app.route("/technology")
+def technology():
+    data = news_categories["technology"]
+    return render_template('homepage.html', data=data, news_length=len(data[0]))@app.route("/sports")
+
+@app.route("/entertainment")
+def entertainment():
+    data = news_categories["entertainment"]
+    return render_template('homepage.html', data=data, news_length=len(data[0]))
+
+@app.route("/science")
+def science():
+    data = news_categories["science"]
+    return render_template('homepage.html', data=data, news_length=len(data[0]))
 
 
 if __name__ == '__main__':
